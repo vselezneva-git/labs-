db5efaae5248716929259b0dc47aaeefb6688b2c
vselezneva-git <bomberlok499@gmail.com>
2024-09-21 21:47:28 +0700

    Word_Count: working commit

---
+++ lab0/lab0(b)/build/CMakeFiles/lab0b.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/OOP_Project/lab0/lab0(b)/App.cpp" "CMakeFiles/lab0b.dir/App.cpp.obj" "gcc" "CMakeFiles/lab0b.dir/App.cpp.obj.d"
+  "C:/OOP_Project/lab0/lab0(b)/Counter.cpp" "CMakeFiles/lab0b.dir/Counter.cpp.obj" "gcc" "CMakeFiles/lab0b.dir/Counter.cpp.obj.d"
+  "C:/OOP_Project/lab0/lab0(b)/Reader.cpp" "CMakeFiles/lab0b.dir/Reader.cpp.obj" "gcc" "CMakeFiles/lab0b.dir/Reader.cpp.obj.d"
+  "C:/OOP_Project/lab0/lab0(b)/Splitting.cpp" "CMakeFiles/lab0b.dir/Splitting.cpp.obj" "gcc" "CMakeFiles/lab0b.dir/Splitting.cpp.obj.d"
+  "C:/OOP_Project/lab0/lab0(b)/Writer.cpp" "CMakeFiles/lab0b.dir/Writer.cpp.obj" "gcc" "CMakeFiles/lab0b.dir/Writer.cpp.obj.d"
+  "C:/OOP_Project/lab0/lab0(b)/main.cpp" "CMakeFiles/lab0b.dir/main.cpp.obj" "gcc" "CMakeFiles/lab0b.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
